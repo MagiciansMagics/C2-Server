9603010b054ed80bd6bc6386b4c35a10cb4bd2eb
MagiciansMagics <150154307+MagiciansMagics@users.noreply.github.com>
2024-08-22 18:40:17 +0300

    Add files via upload

---
+++ QNJ/build.sh
@@ -0,0 +1,11 @@
+g++ main.cpp -o qnj-cloud-server
+echo "Done compiling HTTP and TCP server"
+echo "            ʌ"
+echo "            |"
+echo "            |"
+echo "            |"
+echo "            |"
+echo "            v"
+echo "Compiling proxy server..."
+g++ proxy.cpp -o qnj-proxy-server
+echo "Done compiling proxy server"
